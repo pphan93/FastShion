db2ac5ffece41b6e8de589fb91c3053eb6e5069d
Phuoc Phan <phuocphan12@gmail.com>
2022-03-19 14:01:36 -0400

    working on user page

--- admindashboard/src/App.js
+++ admindashboard/src/App.js
@@ -2,6 +2,8 @@ import SideBar from "./components/sidebar/SideBar";
 import Topbar from "./components/topbar/Topbar";
 import styles from "./App.module.css";
 import Home from "./pages/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from "./pages/user/User";
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
       <div className={styles.container}>
         <SideBar />
         <div className={styles.mainContainer}>
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<User />} />
+          </Routes>
         </div>
       </div>
     </div>
--- admindashboard/src/components/widgetLarge/WidgetLarge.jsx
+++ admindashboard/src/components/widgetLarge/WidgetLarge.jsx
@@ -2,7 +2,71 @@ import React from "react";
 import styles from "./WidgetLarge.module.css";
 
 const WidgetLarge = () => {
-  return <div className={styles.container}>WidgetLarge</div>;
+  const Button = ({ type }) => {
+    return (
+      <button className={`${styles.button} ${styles[type]}`}>{type}</button>
+    );
+  };
+
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.title}>Latest Transactions</h3>
+      <table className={styles.table}>
+        <tr className={styles.tableRow}>
+          <th className={styles.tableHeader}>Customer</th>
+          <th className={styles.tableHeader}>Date</th>
+          <th className={styles.tableHeader}>Amount</th>
+          <th className={styles.tableHeader}>Status</th>
+        </tr>
+        <tr className={styles.tableRow}>
+          <td className={styles.user}>
+            <img
+              src="https://i.pravatar.cc/"
+              alt=""
+              className={styles.img}
+            ></img>
+            <span className={styles.name}>Anna Keller</span>
+          </td>
+          <td className={styles.date}>2 Jne 2021</td>
+          <td className={styles.amount}>$122.00</td>
+          <td className={styles.status}>
+            <Button type="Approved" />
+          </td>
+        </tr>
+
+        <tr className={styles.tableRow}>
+          <td className={styles.user}>
+            <img
+              src="https://i.pravatar.cc/"
+              alt=""
+              className={styles.img}
+            ></img>
+            <span className={styles.name}>Susan Carol</span>
+          </td>
+          <td className={styles.date}>2 Jne 2021</td>
+          <td className={styles.amount}>$122.00</td>
+          <td className={styles.status}>
+            <Button type="Pending" />
+          </td>
+        </tr>
+        <tr className={styles.tableRow}>
+          <td className={styles.user}>
+            <img
+              src="https://i.pravatar.cc/"
+              alt=""
+              className={styles.img}
+            ></img>
+            <span className={styles.name}> Banada Meller</span>
+          </td>
+          <td className={styles.date}>2 Jne 2021</td>
+          <td className={styles.amount}>$1222.00</td>
+          <td className={styles.status}>
+            <Button type="Declined" />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default WidgetLarge;
